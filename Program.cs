8ee29e02e8c6b47a948b033d971177310d55e7fc
Nurlan Memmedov <nurlan.memmedov8818@gmail.com>
2022-11-02 23:52:34 +0400

    Qurup isi hazirdir

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int[] num = {1,2,3,4,5,6,345,432,24,341};
+int left = 0;
+int n = 3;
+int right = num.Length - 1;
+while (left <= right)
+{
+    int middle = (left + right) / 2;
+    if (num[middle] == n)
+    {
+        Console.WriteLine("Axteardiginiz eded tapildi");
+        break;
+    }
+    else if(num[middle] > n)
+    {
+        right = middle - 1;
+    }
+    else
+    {
+        left = middle+1;
+    }
+}
